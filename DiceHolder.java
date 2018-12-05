24d77ba5ce17b6f64a9f0601fc08ab383c2ff59c
Drewsh01 <drewsh01@gmail.com>
2018-12-05 08:09:08 -0500

    addDie made
    Add die made
    no bugs
    passfail made

--- DiceHolder.java
+++ DiceHolder.java
@@ -1,7 +1,23 @@
 import java.util.*;
 	public class DiceHolder{
 
+	private ArrayList <Die> container = new ArrayList();
+	private int passfail = 0;
+		int addDie(Die die){
+			if (container.size() == 6)
+				passfail = -1;
+			if (container.size() != 6){
+				passfail = 1;
+				container.add(die);
+			}
+			return passfail;
+	}
 
-
+		void shake(){
 
 	}
+
+		String toString(){
+
+	}
+}
