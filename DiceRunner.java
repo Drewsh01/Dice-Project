729f39b843020f46a158b18cea8bf24a09257ab1
adhit612 <adhit612@gmail.com>
2018-12-07 07:55:35 -0500

    Worked on test 1
    partially finished test 1

--- DiceRunner.java
+++ DiceRunner.java
@@ -2,8 +2,22 @@ public class DiceRunner{
 	public static void main (String [] args){
 
 Die first = new Die(8);
-first.roll();
-System.out.print(first);
+Die second = new Die(8);
+
+int x = 0;
+
+while(first.getValue() != 1 && second.getValue() != 1){
+	first.roll();
+	second.roll();
+	x ++;
+	System.out.print("Die 1: " + first.getValue() + "Die 2: " + second.getValue());
+
+	if(first.getValue() == 1 && second.getValue() == 1){
+		System.out.println("You got snake eyes, after " + x + " attempts");
+	}
+
+}
+
 
 }
 }
